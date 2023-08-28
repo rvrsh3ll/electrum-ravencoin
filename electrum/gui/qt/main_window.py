20abe765b29c225ac8ace43ef9ff5aab33f1cf67
kralverde <2b2tminecrafter2@gmail.com>
2023-08-28 18:08:35 -0400

    fix update checker

--- electrum/gui/qt/main_window.py
+++ electrum/gui/qt/main_window.py
@@ -754,7 +754,7 @@ class ElectrumWindow(QMainWindow, MessageBoxMixin, Logger, QtEventListener):
         help_menu = menubar.addMenu(_("&Help"))
         help_menu.addAction(_("&About"), self.show_about)
         help_menu.addAction(_("&Check for updates"), self.show_update_check)
-        help_menu.addAction(_("&Official website"), lambda: webopen("https://electrum.org"))
+        help_menu.addAction(_("&Official website"), lambda: webopen("https://github.com/Electrum-RVN-SIG/electrum-ravencoin"))
         help_menu.addSeparator()
         help_menu.addAction(_("&Documentation"), lambda: webopen("http://docs.electrum.org/")).setShortcut(QKeySequence.HelpContents)
         if not constants.net.TESTNET:
