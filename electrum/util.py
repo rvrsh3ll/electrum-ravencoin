47005a18c4650b6df493db3d4dbda68fdd375270
kralverde <2b2tminecrafter2@gmail.com>
2023-09-02 15:06:41 -0400

    update windows data dir

--- electrum/util.py
+++ electrum/util.py
@@ -639,9 +639,9 @@ def user_dir():
     elif os.name == 'posix':
         return os.path.join(os.environ["HOME"], ".electrum-ravencoin")
     elif "APPDATA" in os.environ:
-        return os.path.join(os.environ["APPDATA"], "Electrum")
+        return os.path.join(os.environ["APPDATA"], "Electrum-Ravencoin")
     elif "LOCALAPPDATA" in os.environ:
-        return os.path.join(os.environ["LOCALAPPDATA"], "Electrum")
+        return os.path.join(os.environ["LOCALAPPDATA"], "Electrum-Ravencoin")
     else:
         #raise Exception("No home directory found in environment variables.")
         return
